a20bf3b510e0f675ad295e9b6bea285ddf77ac74
dprezp <126874863+dprezp@users.noreply.github.com>
2025-03-21 19:30:03 +0100

    prueba
    prueba 10

--- Scripts/Prueba_VND.py
+++ Scripts/Prueba_VND.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 import gc #importamos el recolector de basura
 
@@ -77,103 +78,103 @@ data_tuple = (grafic_df, max_time)
 #Definimos diccionario de fairness para tener los valores iniciales de cada uno.
 first_fairness = {}
 
-for i in range(0,10):
-    #Comentar esto para una única ejecución y quitar identados
-    for dataset_index, dataset_used in enumerate(datasets):
-        for attr in protected_attributes[dataset_index]:
 
-            dataset_orig, privileged_groups,unprivileged_groups,optim_options = get_data(dataset_used, attr)
-            lenght_data = dataset_orig.features.shape[0]
+#Comentar esto para una única ejecución y quitar identados
+for dataset_index, dataset_used in enumerate(datasets):
+    for attr in protected_attributes[dataset_index]:
 
-            #------------
-            #Truncamos aleatoriamente a 1000 datos
-            #sample_indices = np.random.choice(len(dataset_orig.features), 400, replace=False)
-            #dataset_orig.features = dataset_orig.features[sample_indices]
-            #dataset_orig.labels = dataset_orig.labels[sample_indices]
-            #------------
+        dataset_orig, privileged_groups,unprivileged_groups,optim_options = get_data(dataset_used, attr)
+        lenght_data = dataset_orig.features.shape[0]
 
-            np.random.seed(1234)
-            dataset_orig_train, dataset_orig_test = dataset_orig.split([0.7], shuffle=True)
-            dataset_orig_test,dataset_orig_valid = dataset_orig_test.split([0.5], shuffle=True)
-            dataset_orig_test_pred = dataset_orig_test.copy(deepcopy=True)
-            dataset_orig_train_pred = dataset_orig_train.copy(deepcopy=True)
-            dataset_orig_valid_pred = dataset_orig_valid.copy(deepcopy=True)
+        #------------
+        #Truncamos aleatoriamente a 1000 datos
+        #sample_indices = np.random.choice(len(dataset_orig.features), 400, replace=False)
+        #dataset_orig.features = dataset_orig.features[sample_indices]
+        #dataset_orig.labels = dataset_orig.labels[sample_indices]
+        #------------
+
+        np.random.seed(1234)
+        dataset_orig_train, dataset_orig_test = dataset_orig.split([0.7], shuffle=True)
+        dataset_orig_test,dataset_orig_valid = dataset_orig_test.split([0.5], shuffle=True)
+        dataset_orig_test_pred = dataset_orig_test.copy(deepcopy=True)
+        dataset_orig_train_pred = dataset_orig_train.copy(deepcopy=True)
+        dataset_orig_valid_pred = dataset_orig_valid.copy(deepcopy=True)
 
 
-            # Creamos el árbol y lo entrenamos
-            clf = tree.DecisionTreeClassifier(random_state=1)
-            clf = clf.fit(dataset_orig_train.features, dataset_orig_train.labels)
-            train_acc,train_aod = get_metrics(clf,dataset_orig_train,dataset_orig_train_pred,unprivileged_groups,privileged_groups)
-            test_acc,test_aod = get_metrics(clf,dataset_orig_test,dataset_orig_test_pred,unprivileged_groups,privileged_groups)
-            valid_acc,valid_aod = get_metrics(clf,dataset_orig_valid,dataset_orig_valid_pred,unprivileged_groups,privileged_groups)
+        # Creamos el árbol y lo entrenamos
+        clf = tree.DecisionTreeClassifier(random_state=1)
+        clf = clf.fit(dataset_orig_train.features, dataset_orig_train.labels)
+        train_acc,train_aod = get_metrics(clf,dataset_orig_train,dataset_orig_train_pred,unprivileged_groups,privileged_groups)
+        test_acc,test_aod = get_metrics(clf,dataset_orig_test,dataset_orig_test_pred,unprivileged_groups,privileged_groups)
+        valid_acc,valid_aod = get_metrics(clf,dataset_orig_valid,dataset_orig_valid_pred,unprivileged_groups,privileged_groups)
+        valid_fair = valid_aod
+
+        metrics_prune_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups, privileged_groups,
+                      dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid, dataset_orig_valid_pred,
+                      "No operator",  [[0]], 0, dataset_used, 0, attr,metrics_prune_df, lenght_data)
+        metrics_relabeling_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups,
+                                         privileged_groups,dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid,
+                                         dataset_orig_valid_pred,"No operator", [[0]], 0, dataset_used, 0, attr, metrics_relabeling_df,lenght_data)
+
+
+        #Guardamos el first fairness en el diccionario
+        first_fairness[(dataset_used,attr)] = valid_fair
+
+
+        #Sacamos métricas de nuestro árbol
+        n_nodes = len(clf.tree_.children_left)
+        leafs = [i for i,x in enumerate(clf.tree_.children_left) if x == -1]
+        interior = [x for x in range(n_nodes) if x not in leafs]
+        prune_count = 0
+
+
+
+        #Array de operator para escribirlo en el documento:
+        operator=["State_of_Art", "First_improvement","Best_improvement"]
+
+        #array de históricos para cada operador
+        hist = [(valid_acc,valid_fair,prune_count)]
+
+
+
+        #Iniciamos el VND
+        i =0
+        global_time = time.time()
+        while(i<2):
+            # copias de arboles e hist para cada iteracion
+            clf_prima = copy.deepcopy(clf)
+            hist_copy = copy.deepcopy(hist)
+
+            if(i==0):
+                clf_prima, data_tuple =  first_improvement_prune(clf_prima,dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups,
+                                                              privileged_groups, hist_copy,data_tuple,dataset_used, attr,1,global_time)
+            elif (i==1):
+                clf_prima,data_tuple =first_improvement_relabeling(clf_prima, dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups,
+                                      privileged_groups, hist_copy,data_tuple, dataset_used, attr,1,global_time)
+
+
+
+            #miramos si mejora:
+            valid_acc, valid_aod = get_metrics(clf_prima, dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups, privileged_groups)
             valid_fair = valid_aod
+            prev_valid_acc, prev_valid_fair, p = hist[-1]
+            if valid_fair < prev_valid_fair and valid_acc >= hist[0][0]:
+                hist.append((valid_acc, valid_fair, prune_count))
+                clf = copy.deepcopy(clf_prima)
+                i = 0
 
-            metrics_prune_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups, privileged_groups,
-                          dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid, dataset_orig_valid_pred,
-                          "No operator",  [[0]], 0, dataset_used, 0, attr,metrics_prune_df, lenght_data)
-            metrics_relabeling_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups,
-                                             privileged_groups,dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid,
-                                             dataset_orig_valid_pred,"No operator", [[0]], 0, dataset_used, 0, attr, metrics_relabeling_df,lenght_data)
-
-
-            #Guardamos el first fairness en el diccionario
-            first_fairness[(dataset_used,attr)] = valid_fair
-
-
-            #Sacamos métricas de nuestro árbol
-            n_nodes = len(clf.tree_.children_left)
-            leafs = [i for i,x in enumerate(clf.tree_.children_left) if x == -1]
-            interior = [x for x in range(n_nodes) if x not in leafs]
-            prune_count = 0
-
-
-
-            #Array de operator para escribirlo en el documento:
-            operator=["State_of_Art", "First_improvement","Best_improvement"]
-
-            #array de históricos para cada operador
-            hist = [(valid_acc,valid_fair,prune_count)]
-
-
-
-            #Iniciamos el VND
-            i =0
-            global_time = time.time()
-            while(i<2):
-                # copias de arboles e hist para cada iteracion
-                clf_prima = copy.deepcopy(clf)
-                hist_copy = copy.deepcopy(hist)
-
-                if(i==0):
-                    clf_prima, data_tuple =  first_improvement_prune(clf_prima,dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups,
-                                                                  privileged_groups, hist_copy,data_tuple,dataset_used, attr,1,global_time)
-                elif (i==1):
-                    clf_prima,data_tuple =first_improvement_relabeling(clf_prima, dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups,
-                                          privileged_groups, hist_copy,data_tuple, dataset_used, attr,1,global_time)
-
-
-
-                #miramos si mejora:
-                valid_acc, valid_aod = get_metrics(clf_prima, dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups, privileged_groups)
-                valid_fair = valid_aod
-                prev_valid_acc, prev_valid_fair, p = hist[-1]
-                if valid_fair < prev_valid_fair and valid_acc >= hist[0][0]:
-                    hist.append((valid_acc, valid_fair, prune_count))
-                    clf = copy.deepcopy(clf_prima)
-                    i = 0
-
-                else:
-                    i = i + 1
+            else:
+                i = i + 1
 
 
 
 
 
-    #Guardamos el Excel
+#Guardamos el Excel
 
-    prueba_df, invalid = data_tuple
-    excel_path = os.path.join("Results", "results_{}.xlsx".format(i))
-    prueba_df.to_excel(excel_path)
+prueba_df, invalid = data_tuple
+excel_path = os.path.join("Results", "results_{}.xlsx".format(datetime.datetime.now()))
+prueba_df.to_excel(excel_path)
 
 
 
--- Scripts/Prueba_first.py
+++ Scripts/Prueba_first.py
@@ -1,5 +1,8 @@
 import os
 import gc #importamos el recolector de basura
+import datetime
+
+from PIL.TiffImagePlugin import DATE_TIME
 
 os.environ["OMP_NUM_THREADS"] = "1" # export OMP_NUM_THREADS=1
 os.environ["OPENBLAS_NUM_THREADS"] = "1" # export OPENBLAS_NUM_THREADS=1
@@ -77,102 +80,101 @@ data_tuple = (grafic_df, max_time)
 #Definimos diccionario de fairness para tener los valores iniciales de cada uno.
 first_fairness = {}
 
-for i in range(0,10):
 
-    #Comentar esto para una única ejecución y quitar identados
-    for dataset_index, dataset_used in enumerate(datasets):
-        for attr in protected_attributes[dataset_index]:
+#Comentar esto para una única ejecución y quitar identados
+for dataset_index, dataset_used in enumerate(datasets):
+    for attr in protected_attributes[dataset_index]:
 
-            dataset_orig, privileged_groups,unprivileged_groups,optim_options = get_data(dataset_used, attr)
-            lenght_data = dataset_orig.features.shape[0]
+        dataset_orig, privileged_groups,unprivileged_groups,optim_options = get_data(dataset_used, attr)
+        lenght_data = dataset_orig.features.shape[0]
 
-            #------------
-            #Truncamos aleatoriamente a 1000 datos
-            #sample_indices = np.random.choice(len(dataset_orig.features), 100, replace=False)
-            #dataset_orig.features = dataset_orig.features[sample_indices]
-            #dataset_orig.labels = dataset_orig.labels[sample_indices]
-            #------------
+        #------------
+        #Truncamos aleatoriamente a 1000 datos
+        #sample_indices = np.random.choice(len(dataset_orig.features), 100, replace=False)
+        #dataset_orig.features = dataset_orig.features[sample_indices]
+        #dataset_orig.labels = dataset_orig.labels[sample_indices]
+        #------------
 
-            np.random.seed(1234)
-            dataset_orig_train, dataset_orig_test = dataset_orig.split([0.7], shuffle=True)
-            dataset_orig_test,dataset_orig_valid = dataset_orig_test.split([0.5], shuffle=True)
-            dataset_orig_test_pred = dataset_orig_test.copy(deepcopy=True)
-            dataset_orig_train_pred = dataset_orig_train.copy(deepcopy=True)
-            dataset_orig_valid_pred = dataset_orig_valid.copy(deepcopy=True)
-
-
-            # Creamos el árbol y lo entrenamos
-            clf = tree.DecisionTreeClassifier(random_state=1)
-            clf = clf.fit(dataset_orig_train.features, dataset_orig_train.labels)
-            train_acc,train_aod = get_metrics(clf,dataset_orig_train,dataset_orig_train_pred,unprivileged_groups,privileged_groups)
-            test_acc,test_aod = get_metrics(clf,dataset_orig_test,dataset_orig_test_pred,unprivileged_groups,privileged_groups)
-            valid_acc,valid_aod = get_metrics(clf,dataset_orig_valid,dataset_orig_valid_pred,unprivileged_groups,privileged_groups)
-            valid_fair = valid_aod
-
-            metrics_prune_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups, privileged_groups,
-                          dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid, dataset_orig_valid_pred,
-                          "No operator",  [[0]], 0, dataset_used, 0, attr,metrics_prune_df, lenght_data)
-            metrics_relabeling_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups,
-                                             privileged_groups,dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid,
-                                             dataset_orig_valid_pred,"No operator", [[0]], 0, dataset_used, 0, attr, metrics_relabeling_df,lenght_data)
+        np.random.seed(1234)
+        dataset_orig_train, dataset_orig_test = dataset_orig.split([0.7], shuffle=True)
+        dataset_orig_test,dataset_orig_valid = dataset_orig_test.split([0.5], shuffle=True)
+        dataset_orig_test_pred = dataset_orig_test.copy(deepcopy=True)
+        dataset_orig_train_pred = dataset_orig_train.copy(deepcopy=True)
+        dataset_orig_valid_pred = dataset_orig_valid.copy(deepcopy=True)
 
 
-            #Guardamos el first fairness en el diccionario
-            first_fairness[(dataset_used,attr)] = valid_fair
+        # Creamos el árbol y lo entrenamos
+        clf = tree.DecisionTreeClassifier(random_state=1)
+        clf = clf.fit(dataset_orig_train.features, dataset_orig_train.labels)
+        train_acc,train_aod = get_metrics(clf,dataset_orig_train,dataset_orig_train_pred,unprivileged_groups,privileged_groups)
+        test_acc,test_aod = get_metrics(clf,dataset_orig_test,dataset_orig_test_pred,unprivileged_groups,privileged_groups)
+        valid_acc,valid_aod = get_metrics(clf,dataset_orig_valid,dataset_orig_valid_pred,unprivileged_groups,privileged_groups)
+        valid_fair = valid_aod
+
+        metrics_prune_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups, privileged_groups,
+                      dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid, dataset_orig_valid_pred,
+                      "No operator",  [[0]], 0, dataset_used, 0, attr,metrics_prune_df, lenght_data)
+        metrics_relabeling_df = write_metrics(clf, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups,
+                                         privileged_groups,dataset_orig_test, dataset_orig_test_pred, dataset_orig_valid,
+                                         dataset_orig_valid_pred,"No operator", [[0]], 0, dataset_used, 0, attr, metrics_relabeling_df,lenght_data)
 
 
-            #Sacamos métricas de nuestro árbol
-            n_nodes = len(clf.tree_.children_left)
-            leafs = [i for i,x in enumerate(clf.tree_.children_left) if x == -1]
-            interior = [x for x in range(n_nodes) if x not in leafs]
-            prune_count = 0
-
-            #copias de arboles para cada operador
-            clf_first_prune = copy.deepcopy(clf)
+        #Guardamos el first fairness en el diccionario
+        first_fairness[(dataset_used,attr)] = valid_fair
 
 
-            #Array de operator para escribirlo en el documento:
-            operator=["State_of_Art", "First_improvement","Best_improvement"]
+        #Sacamos métricas de nuestro árbol
+        n_nodes = len(clf.tree_.children_left)
+        leafs = [i for i,x in enumerate(clf.tree_.children_left) if x == -1]
+        interior = [x for x in range(n_nodes) if x not in leafs]
+        prune_count = 0
 
-            #array de históricos para cada operador
-            hist_art = [(valid_acc,valid_fair,prune_count)]
-            hist_first_prune = [(valid_acc, valid_fair, prune_count)]
+        #copias de arboles para cada operador
+        clf_first_prune = copy.deepcopy(clf)
 
 
+        #Array de operator para escribirlo en el documento:
+        operator=["State_of_Art", "First_improvement","Best_improvement"]
 
-            #Pruning, first improvement
-            start_time = time.time()
-            clf_first_prune, data_tuple = first_improvement_prune(clf_first_prune,dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups,
-                                                                  privileged_groups, hist_first_prune,data_tuple,dataset_used, attr,0,None)
-            elapsed_time = time.time() - start_time
-            n_nodes = len(clf_first_prune.tree_.children_left)
-            metrics_prune_df = write_metrics(clf_first_prune, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups, privileged_groups,dataset_orig_test,
-                          dataset_orig_test_pred, dataset_orig_valid, dataset_orig_valid_pred, operator[1],hist_first_prune, n_nodes,
-                          dataset_used,elapsed_time,attr, metrics_prune_df,lenght_data)
+        #array de históricos para cada operador
+        hist_art = [(valid_acc,valid_fair,prune_count)]
+        hist_first_prune = [(valid_acc, valid_fair, prune_count)]
 
 
 
-            #Borramos info del segundo metodo
-            del clf_first_prune,hist_first_prune
-            gc.collect()
+        #Pruning, first improvement
+        start_time = time.time()
+        clf_first_prune, data_tuple = first_improvement_prune(clf_first_prune,dataset_orig_valid, dataset_orig_valid_pred, unprivileged_groups,
+                                                              privileged_groups, hist_first_prune,data_tuple,dataset_used, attr,0,None)
+        elapsed_time = time.time() - start_time
+        n_nodes = len(clf_first_prune.tree_.children_left)
+        metrics_prune_df = write_metrics(clf_first_prune, dataset_orig_train, dataset_orig_train_pred, unprivileged_groups, privileged_groups,dataset_orig_test,
+                      dataset_orig_test_pred, dataset_orig_valid, dataset_orig_valid_pred, operator[1],hist_first_prune, n_nodes,
+                      dataset_used,elapsed_time,attr, metrics_prune_df,lenght_data)
 
 
 
-    #Guardamos el Excel
-    excel_path = os.path.join("Results", "metrics_Prune_results_{}.xlsx".format(i))
-    metrics_prune_df.to_excel(excel_path)
+        #Borramos info del segundo metodo
+        del clf_first_prune,hist_first_prune
+        gc.collect()
 
-    prueba_df, invalid = data_tuple
-    excel_path = os.path.join("Results", "prueba_{}.xlsx".format(i))
-    prueba_df.to_excel(excel_path)
 
-    grafic_df = table_align(data_tuple, first_fairness)
 
-    csv_path = os.path.join("Results", "backup_{}.csv".format(i))
-    grafic_df.to_csv(excel_path, index=False)
+#Guardamos el Excel
+excel_path = os.path.join("Results", "metrics_Prune_results_{}.xlsx".format(datetime.datetime.now()))
+metrics_prune_df.to_excel(excel_path)
 
-    excel_path = os.path.join("Results", "grafic_metrics_{}.xlsx".format(i))
-    grafic_df.to_excel(excel_path)
+prueba_df, invalid = data_tuple
+excel_path = os.path.join("Results", "prueba_{}.xlsx".format(datetime.datetime.now()))
+prueba_df.to_excel(excel_path)
+
+grafic_df = table_align(data_tuple, first_fairness)
+
+csv_path = os.path.join("Results", "backup_{}.csv".format(datetime.datetime.now()))
+grafic_df.to_csv(excel_path, index=False)
+
+excel_path = os.path.join("Results", "grafic_metrics_{}.xlsx".format(datetime.datetime.now()))
+grafic_df.to_excel(excel_path)
 
 
 
